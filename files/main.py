71ddea07716be51dcceefd098c62716e1e45a5c2
danielsagher <daniel.sagher1@gmail.com>
2024-08-01 16:14:24 -0400

    BOOM. It works baby. Will do test files now and close it out

--- files/main.py
+++ files/main.py
@@ -5,26 +5,6 @@ from get_sub_type import dataset
 from re import fullmatch
 from push import sql_push
 
-'''
-
-main() will call arg to ask user for file and arguments. 
-file will then be passed into:
-
-extract_unique_values() - adds column names and unique values to the DataSet class and returns a list of boolean columns
-del_index() -
-
-get_type()- get_type() will return statements of general data type classification to get_sub_type().
-
-get_sub_type() - will determine which subtype to use, and/or ask the user what they want to use based off what options they have.
-                results will be passed to generate_ddl()
-
-generate_ddl() - will generate SQL data definition language and pass result to push()
-
-push()- will use SQLalchemy to push to a Postgres server. 
-
-'''
-
-
 def main():
 
     # Open file and assign to lst as list[dicts]
@@ -66,10 +46,9 @@ def main():
         answer = input('No index detected. Would you like to add one? (yes/y to confirm) ')
         want_index = True if answer.lower() in ['yes', 'y'] else False
 
-    print(dataset.dicts)
-    column_lst = generate_ddl(response, want_index, precision_decision)
-    sql_push(column_lst)
-    
 
+    column_lst = generate_ddl(response, want_index, precision_decision)
+    sql_push(column_lst, lst)
+    
 if __name__ == '__main__':
     main()
--- files/push.py
+++ files/push.py
@@ -1,8 +1,6 @@
-import psycopg2 as ps
-from generate_ddl import dataset
+import sqlalchemy as sa
 
-
-def sql_push(column_lst):
+def sql_push(column_lst, lst):
     host = input('Host: ')
     dbname = input('DB name: ')
     user = input('User: ')
@@ -10,38 +8,60 @@ def sql_push(column_lst):
     port = input('Port: ')
     table_name = input('Table Name: ')
 
-    
-    conn = ps.connect(host=host, dbname=dbname, user=user, 
-                password=password, port=port)
-    cur = conn.cursor()
+    db_url = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
 
-    
-    column_definitions = ", ".join(column_lst)
+    engine = sa.create_engine(db_url)
+    connection = engine.connect()
 
-    create_sql = f'''
-                --sql
-                CREATE TABLE IF NOT EXISTS {table_name} (
-                {column_definitions}
-                );   
-        '''
+    metadata = sa.MetaData()
+
+    columns = []
+    for col in column_lst:
+        col_name, col_type = col.split(' ', 1)
+        
+        if col_type.startswith('CHAR'):
+            char_length = int(col_type.split('(')[1].strip(')'))
+            col_type_obj = sa.CHAR(char_length)
+        elif col_type.startswith('VARCHAR'):
+            col_type_obj = sa.String
+        elif col_type == 'TEXT':
+            col_type_obj = sa.Text
+        elif col_type == 'BOOLEAN':
+            col_type_obj = sa.Boolean
+        elif col_type == 'DATE':
+            col_type_obj = sa.Date
+        elif col_type == 'TIME':
+            col_type_obj = sa.Time
+        elif col_type == 'TIMESTAMP':
+            col_type_obj = sa.TIMESTAMP
+        elif col_type == 'SMALLINT':
+            col_type_obj = sa.SmallInteger
+        elif col_type == 'INT' or col_type == 'INTEGER':
+            col_type_obj = sa.Integer
+        elif col_type == 'BIGINT':
+            col_type_obj = sa.BigInteger
+        elif col_type == 'REAL':
+            col_type_obj = sa.REAL
+        elif col_type == 'DOUBLE PRECISION':
+            col_type_obj = sa.Float  # SQLAlchemy uses Float for double precision
+        elif col_type.startswith('NUMERIC'):
+                col_type_obj = sa.Numeric
+        else:
+            col_type_obj = sa.String  # Default type if not recognized
+
+        columns.append(sa.Column(col_name, col_type_obj))
+          
+    table = sa.Table(table_name,metadata,*columns)
+
+    metadata.create_all(engine)
+
     try:
-        cur.execute(create_sql)
-
-        # Need to add placeholders
-        for key in dataset.dicts:
-            value_lst = [(v,) for v in dataset.dicts[key]]
-            cur.executemany(f'INSERT INTO {table_name} ({key}) VALUES (%s)', value_lst)
-
-        conn.commit()
-
-
+        with engine.begin() as conn:
+            conn.execute(table.insert(), lst)
+        print(f"Data inserted successfully into {table_name}.")
     except Exception as e:
-        print(f'An error occured: {e}')
+        print(f'An error occurred: {e}')
         conn.rollback()
 
     finally:
-        cur.close()
-        conn.close()
-    
-
-
+        connection.close()
